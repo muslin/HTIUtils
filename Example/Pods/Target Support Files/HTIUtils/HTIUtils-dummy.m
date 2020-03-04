a7b1b39f4898c8dc9cba3bcbb3625c8f0f71634c
muslin <muslin@hytexts.com>
2020-03-04 11:17:58 +0700

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/HTIUtils/HTIUtils-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HTIUtils : NSObject
+@end
+@implementation PodsDummy_HTIUtils
+@end
---
+++ Example/Pods/Target Support Files/Pods-HTIUtils_Tests/Pods-HTIUtils_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HTIUtils_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_HTIUtils_Tests
+@end
